87efd7a842464d1aa613c85417746176e169d8d1
Nathan <nate1898@gmail.com>
2020-10-09 16:28:17 -0400

    Stretch

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -4,7 +4,17 @@ import sys
 
 def rock_paper_scissors(n):
   # Your code here
+  plays = ['rock', 'paper', 'scissors']
+  result = []
 
+  def outcomes(playsSoFar, roundsLeft):
+    if roundsLeft == 0:
+      result.append(playsSoFar)
+    else:
+      for i in range(3):
+        outcomes(playsSoFar + [plays[i]], roundsLeft - 1)
+  outcomes([], n)
+  return result
   pass
 
 
